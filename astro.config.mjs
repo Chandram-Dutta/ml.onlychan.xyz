3242ebd43be8d85ad7c7b5b15d401d792f52afab
Chandram-Dutta <chandramdutta2004@gmail.com>
2025-03-31 22:02:39 +0530

    feat: added existing blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  ],
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  server: {
+    host: '0.0.0.0'
+  },
+});
